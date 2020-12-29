aa021fdd77cdb4985ed11f9546a507a984d46b10
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2020-12-29 02:49:09 -0500

    Only 2 files in the web app.

---
+++ domains.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+Domain Information for <?php echo $_GET["domain_name"]; ?><br>
+
+</body>
+</html> 
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<form action="domains.php" method="get">
+Domain Name: <input type="text" name="domain_name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
