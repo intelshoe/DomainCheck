864840ab3968dab863802b46caa2738812a31719
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2020-12-29 05:52:07 -0500

    add style and reformated

--- index.php
+++ index.php
@@ -1,23 +1,51 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Domain Info</title>
+	<meta charset="utf-8">
+	<title>Domain Info Tool</title>
+
+	<link rel="stylesheet" media="screen" href="styles.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Ryan Willis">
+	<meta name="description" content="Domain information related to hosting and performance.">
+	
+	<script src="https://use.typekit.net/vzq4ipz.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+
+	<!--[if lt IE 9]>
+	<script src="script/html5shiv.js"></script>
+	<![endif]-->
 </head>
-<body>
 
-<!-- Domains Search box and submit button -->
-<div style="text-align: center;"><h1>Domain Lookup</h1>
-<form action="domains.php" method="get">
-Domain Name: <input type="text" name="domain_name"><br>
-<input type="submit">
-</form>
-</div>
+<body id="css-zen-garden">
+<div class="page-wrapper">
 
-<h3>About This Tool</h3>
-<p>I'm lazy so I made this to show as much info about a domain on one page as possible.  It also has direct links to check sites using other services at the bottom.  If you have suggestions please email info@stackevolve.com or find me in Slack.</p>
-<p>Take care,</br>
-Ryan W.</p>
+	<section class="intro" id="zen-intro">
+		<header role="banner">
+			<h1>Domain Info Tool</h1>
+			<h2>Show useful hosting information <abbr title="Cascading Style Sheets">on one page</abbr> .</h2>
+		</header>
 
+		<div class="summary" id="zen-summary" role="article">
+			<!-- Domains Search box and submit button -->
+			<h1>Domain Lookup</h1>
+			<form action="domains.php" method="get">
+			Domain Name: <input type="text" name="domain_name">
+			<input type="submit" value="Submit">
+			</form>
+		</div>
+
+	</section>
+	<div style="margin-top:5.5em;" class="main supporting" id="zen-supporting" role="main">
+		<div class="explanation" id="zen-explanation" role="article">
+			<h3 style="margin-top:-50px;">Feedback is Welcome</h3>
+			<p style="margin-top:40px; margin-bottom:50px;" >If you have suggestions feel free to email or find me on Slack.<br><br>
+			Take care,<br>
+			Ryan W.<br>
+			info@stackevolve.com</p>
+		</div>
+		</div>
 
 </body>
 </html> 
