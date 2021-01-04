f7df29addae65d74295c9860fd9f2c1b471ad0f2
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-04 05:49:56 -0500

    minor update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="author" content="Ryan Willis">
 	<meta name="description" content="Domain information related to hosting and performance.">
-	
+
 </head>
 
 <body id="css-zen-garden">
@@ -27,15 +27,15 @@
 			<form action="domains.php" method="get">
 			Domain Name: <input type="text" name="domain_name">
 			<input type="submit" value="Submit">
-			</form> <br><br><br><br>
-			<ul style="max-width: 420px;"><li>To use this website by highlighting a domain name and pressing F11: <a href="hotkey.txt" target='_blank'>Add this code to your Hotkey script.</a></li></ul>
+			</form> <br><br>
+			<ul style="max-width: 420px;"><li>Please enter domain as example.com</li><li>To use this website by highlighting a domain name and pressing F11, <a href="hotkey.txt" target='_blank'>add this code to your Hotkey script.</a></li></ul>
 		</div>
 
 	</section>
 	<div style="margin-top:5.5em;" class="main supporting" id="zen-supporting" role="main">
 		<div class="explanation" id="zen-explanation" role="article">
 			<h3 style="margin-top:-50px;">Feedback is Welcome</h3>
-			<p style="margin-top:40px; margin-bottom:50px;" >If you have suggestions feel free to email or find me on Slack.<br><br> 
+			<p style="margin-top:30px; margin-bottom:18px;" >If you have suggestions feel free to email or find me on Slack.<br><br> 
 			<a style="color: #ffc35d;" href="https://github.com/mellowpuppy/DomainCheck" target='_blank'>>> View the code on Github. <<</a><br><br>
 			Take care,<br>
 			Ryan Wi.<br>
