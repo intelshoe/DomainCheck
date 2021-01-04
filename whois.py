1252f60ef054f3c702f7af273d3d5efb15a2116c
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-03 20:24:19 -0500

    added whitespace in output

--- whois.py
+++ whois.py
@@ -11,38 +11,38 @@ text = subprocess.run(
 # convert whois output to string
 output = text.stdout
 str_out = str(output)
-
-# show domain status text in whois output
-status = "Domain Status: "
-showStatus = str_out.find(status)
-if showStatus == -1:
-    print("Domain Status: not found <br>")
-else:
-    print(str_out[showStatus: showStatus + 40] + " <br>")
     
 # show domain registrar text in whois output
 regis = "Registrar: "
 showReg = str_out.find(regis)
 if showReg == -1:
-    print("Registrar: not found <br>")
+    print("Registrar: not found <br><br>")
 else:
-    print(str_out[showReg: showReg + 28] + " <br>")
+    print(str_out[showReg: showReg + 28] + " <br><br>")
 
 # show domain registrar url text in whois output
 regurl = "Registrar URL: "
 showRegURL = str_out.find(regurl)
 if showRegURL == -1:
-    print("Registrar URL: not found <br>")
+    print("Registrar URL: not found <br><br>")
 else:
-    print(str_out[showRegURL: showRegURL + 40] + " <br>")
+    print(str_out[showRegURL: showRegURL + 40] + " <br><br>")
+    
+# show domain status text in whois output
+status = "Domain Status: "
+showStatus = str_out.find(status)
+if showStatus == -1:
+    print("Domain Status: not found <br><br>")
+else:
+    print(str_out[showStatus: showStatus + 40] + " <br><br>")
 
 # show domain creation date
 creation = "Creation Date: "
 showCreation = str_out.find(creation)
 if showCreation == -1:
-    print("Creation Date: not found <br>")
+    print("Creation Date: not found <br><br>")
 else:
-    print(str_out[showCreation: showCreation + 25] + " <br>")
+    print(str_out[showCreation: showCreation + 25] + " <br><br>")
     
 # show domain expiration date
 expdate = "Registration Expiration Date: "
@@ -51,7 +51,7 @@ if showExp == -1:
     expdate2 = "Expiry Date: "
     showExp2 = str_out.find(expdate2)
     if showExp2 == -1:
-        print("Expiry Date: not found <br>")
+        print("Expiry Date: not found <br><br>")
     else:
        print(str_out[showExp2: showExp2 + 23] + " <br><br>") 
 else:
@@ -61,7 +61,7 @@ else:
 d_ns = "Name Server: "
 showNS = str_out.find(d_ns)
 if showExp == -1:
-    print("Name Server: not found <br>")
+    print("Name Server: not found <br><br>")
 else:
     print(str_out[showNS: showNS + 73] + " <br><br>")
     
